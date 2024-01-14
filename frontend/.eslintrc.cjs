f3c709fc8bc2ee39e636893b83b2aa28bf549664
Oyeins-GUI <oyeinx7@gmail.com>
2024-01-15 00:02:08 +0100

    added parserOptions.project

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,20 +1,20 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+   root: true,
+   env: { browser: true, es2020: true },
+   extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended-type-checked",
+      "plugin:react-hooks/recommended",
+      "plugin:react/recommended",
+      "plugin:react/jsx-runtime",
+   ],
+   ignorePatterns: ["dist", ".eslintrc.cjs"],
+   parser: "@typescript-eslint/parser",
+   parserOptions: {
+      project: ["./tsconfig.json"], // Specify it only for TypeScript files
+   },
+   plugins: ["react-refresh"],
+   rules: {
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+   },
 }
