d710305c8c9107a038def754310243b42f245bf2
Oyeins-GUI <oyeinx7@gmail.com>
2024-02-16 23:07:06 +0100

    added not signed in conditional

--- frontend/src/utils/truncateAddress.ts
+++ frontend/src/utils/truncateAddress.ts
@@ -1,4 +1,8 @@
+import { userSession } from "./authenticate"
+
 export const truncateAddress = (address: string) => {
+   if (!userSession.isUserSignedIn()) return
+
    const charsInAddress = address.split("")
    charsInAddress.splice(4, 33, "...")
 
