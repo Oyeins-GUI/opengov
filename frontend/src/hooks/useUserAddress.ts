d0e7b1369ea4e61ad077a3e632cd03f9721289b0
Oyeins-GUI <oyeinx7@gmail.com>
2024-02-16 23:02:05 +0100

    added not signed in conditional

--- frontend/src/hooks/useUserAddress.ts
+++ frontend/src/hooks/useUserAddress.ts
@@ -1,10 +1,9 @@
-import { userSession } from "../utils/authenticate"
-import { truncateAddress } from "../utils/truncateAddress"
+import { userSession } from "@/utils/authenticate"
 
 export default function useUserAdress(chain: string) {
    if (!userSession.isUserSignedIn()) return
    const testnetAddress = userSession.loadUserData().profile.stxAddress.testnet
    const mainnetAddress = userSession.loadUserData().profile.stxAddress.mainnet
 
-   return chain === "mainnet" ? truncateAddress(mainnetAddress) : truncateAddress(testnetAddress)
+   return chain === "mainnet" ? mainnetAddress : testnetAddress
 }
