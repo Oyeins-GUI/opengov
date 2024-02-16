728a3fadcb58b7088f7189699100ab950644c6f3
Oyeins-GUI <oyeinx7@gmail.com>
2024-02-16 23:08:32 +0100

    changed data flow

--- frontend/src/utils/createProposalContractCall.ts
+++ frontend/src/utils/createProposalContractCall.ts
@@ -1,31 +1,40 @@
 import { openContractCall } from "@stacks/connect-react"
-import { storage, userSession } from "./authenticate"
+import { userSession } from "./authenticate"
 import { AnchorMode, stringAsciiCV } from "@stacks/transactions"
 import { StacksMainnet, StacksTestnet } from "@stacks/network"
 import { DefaultFormValues } from "@/types"
+// import { sha256 } from "js-sha256"
+import { databases, databaseId, proposalCollection, ID } from "@/lib/appwrite"
 
 type Chain = "mainnet" | "testnet" | "devnet"
 
 export async function createProposalContractCall(data: DefaultFormValues, chain: Chain) {
-   console.log(data)
+   // const hash = sha256(JSON.stringify(data))
 
-   const txOptions = {
-      contractAddress: "ST16FECHZJPM4Z95D0Y2G7MSPGK0JHHCAE3JT049N",
-      contractName: "open-gov-v4",
-      functionName: "create-proposal",
-      functionArgs: [stringAsciiCV("title"), stringAsciiCV("niche"), stringAsciiCV("description")],
-      senderKey: userSession.loadUserData().appPrivateKey,
-      validateWithAbi: true,
-      network: chain === "mainnet" ? new StacksMainnet() : new StacksTestnet(),
-      postConditions: [],
-      anchorMode: AnchorMode.Any,
-      onFinish: (txData: { txId: unknown }) => {
-         console.log("Transaction ID:", `0x${txData.txId}`)
-         const fileUrl = storage.putFile("proposal.json", JSON.stringify(data))
+   const createDocument = await databases.createDocument(databaseId, proposalCollection, ID.unique(), data)
 
-         console.log(fileUrl)
-      },
+   if (!createDocument.$id) return
+
+   if (createDocument.$id) {
+      const txOptions = {
+         contractAddress: "ST16FECHZJPM4Z95D0Y2G7MSPGK0JHHCAE3JT049N",
+         contractName: "open-gov-v4",
+         functionName: "create-proposal",
+         functionArgs: [stringAsciiCV("title"), stringAsciiCV("niche"), stringAsciiCV("description")],
+         senderKey: userSession.loadUserData().appPrivateKey,
+         validateWithAbi: true,
+         network: chain === "mainnet" ? new StacksMainnet() : new StacksTestnet(),
+         postConditions: [],
+         anchorMode: AnchorMode.Any,
+         onFinish: async (txData: { txId: unknown }) => {
+            window.open(`https://explorer.hiro.so/txid/0x${txData.txId}?chain=testnet`)
+            window.location.reload()
+         },
+         onCancel: () => {
+            alert("You cancelled the transaction")
+         },
+      }
+
+      await openContractCall(txOptions)
    }
-
-   await openContractCall(txOptions)
 }
