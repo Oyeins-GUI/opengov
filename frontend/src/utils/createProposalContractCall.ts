8b5381df9ba52585b5075ac21465b2ba52ec7ee3
Oyeins-GUI <oyeinx7@gmail.com>
2024-03-20 00:34:45 +0100

    generated types with clarity-types

--- frontend/src/utils/createProposalContractCall.ts
+++ frontend/src/utils/createProposalContractCall.ts
@@ -1,16 +1,24 @@
 import { openContractCall } from "@stacks/connect-react"
 import { userSession } from "./authenticate"
-import { AnchorMode, stringAsciiCV } from "@stacks/transactions"
+import { AnchorMode, bufferCV, stringAsciiCV } from "@stacks/transactions"
 import { StacksMainnet, StacksTestnet } from "@stacks/network"
 import { DefaultFormValues } from "@/types"
-// import { sha256 } from "js-sha256"
+import { sha256 } from "js-sha256"
 import { databases, databaseId, proposalCollection, ID } from "@/lib/appwrite"
-import getTxUpdate, { getTxUpdateWs } from "./getTxUpdate"
+
+import { defineContract } from "clarity-types"
+import { OpengovMainContract } from "../../../contract/contracts/types/opengov"
 
 export type Chain = "mainnet" | "testnet" | "devnet"
 
 export async function createProposalContractCall(data: DefaultFormValues, chain: Chain) {
-   // const hash = sha256(JSON.stringify(data))
+   const openGovContract = defineContract<OpengovMainContract>({
+      contractAddress: "ST16FECHZJPM4Z95D0Y2G7MSPGK0JHHCAE3JT049N",
+      contractName: "opengov-alpha",
+   })
+
+   const hash = sha256(JSON.stringify(data))
+   const uint8Array = new Uint8Array(hash.length)
 
    const network = chain === "mainnet" ? new StacksMainnet() : new StacksTestnet()
 
@@ -19,34 +27,24 @@ export async function createProposalContractCall(data: DefaultFormValues, chain:
    if (!createDocument.$id) return
 
    if (createDocument.$id) {
-      const txOptions = {
-         contractAddress: "ST16FECHZJPM4Z95D0Y2G7MSPGK0JHHCAE3JT049N",
-         contractName: "open-gov-v4",
-         functionName: "create-proposal",
-         functionArgs: [stringAsciiCV("title"), stringAsciiCV("niche"), stringAsciiCV("description")],
+      await openContractCall({
+         ...openGovContract.callOptions({
+            functionName: "create-proposal",
+            functionArgs: [stringAsciiCV(createDocument.$id), bufferCV(uint8Array)],
+         }),
          senderKey: userSession.loadUserData().appPrivateKey,
-         validateWithAbi: true,
          network,
          postConditions: [],
          anchorMode: AnchorMode.Any,
          onFinish: async (txData: { txId: unknown }) => {
             window.open(`https://explorer.hiro.so/txid/${txData.txId}?chain=testnet`)
-
-            getTxUpdateWs(`${txData.txId}`, databaseId, proposalCollection, createDocument.$id, chain)
-
-            setInterval(
-               () =>
-                  getTxUpdate(`${txData.txId}`, network.coreApiUrl, databaseId, proposalCollection, createDocument.$id),
-               60000,
-            )
+            alert("Your proposal will show up once the tx is confirmed")
          },
          onCancel: async () => {
             await databases.deleteDocument(databaseId, proposalCollection, createDocument.$id)
 
             alert("You cancelled the transaction")
          },
-      }
-
-      await openContractCall(txOptions)
+      })
    }
 }
