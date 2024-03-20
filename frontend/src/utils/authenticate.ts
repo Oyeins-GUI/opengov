01695465dc7c21af21b5cd3d2bb2cea07c4badf1
Oyeins-GUI <oyeinx7@gmail.com>
2024-03-20 01:50:15 +0100

    removed unused conditional

--- frontend/src/utils/authenticate.ts
+++ frontend/src/utils/authenticate.ts
@@ -1,20 +1,8 @@
 import { AppConfig, UserSession, showConnect } from "@stacks/connect"
 import logo from "../assets/opengov-logo2.png"
-import { Storage } from "@stacks/storage"
 
 const appConfig = new AppConfig(["store_write", "publish_data"])
 export const userSession = new UserSession({ appConfig })
-
-if (userSession.isUserSignedIn()) {
-   const privateKey = userSession.loadUserData().appPrivateKey
-   const userAppData = userSession.store.getSessionData().userData
-   userSession.store.getSessionData().userData = {
-      appPrivateKey: privateKey,
-      ...userAppData,
-   }
-}
-
-export const storage = new Storage({ userSession })
 
 export function authenticate() {
    showConnect({
