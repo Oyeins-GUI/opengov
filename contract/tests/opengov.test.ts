0f10d3c802c1d8e5a9cd7584fca65db230014fef
Oyeins-GUI <oyeinx7@gmail.com>
2024-09-30 23:29:58 +0100

    added test for the delegate function in the contract

--- contract/tests/opengov.test.ts
+++ contract/tests/opengov.test.ts
@@ -6,61 +6,131 @@ const deployer = accounts.get("deployer")!;
 const address1 = accounts.get("wallet_1")!;
 const address2 = accounts.get("wallet_2")!;
 
-const id = 0
-const title = "Open Gov"
-const niche = "Governance"
-const description = "Open Gov is a crowfunding platform on stacks blockchain"
-const amountNeeded = 2500
-const additionalResource = "https://docs.google.com/document/d/1OvPaYiKWuSkFCnEyufrXUVHeYhGphzysVz-gdV3JQ4c"
-const amount = 2000000
+const id = 0;
+const title = "Open Gov";
+const niche = "Governance";
+const problem =
+   "There is no platform available for projects that are not yet ready for funding.";
+const solution = "Creating that one platform";
+const milestone = "Q1 & Q3";
+const twitter = "@oyeins7";
+const discord = "oyeins.dev";
+const amountNeeded = 2500;
+const additionalResource =
+   "https://docs.google.com/document/d/1OvPaYiKWuSkFCnEyufrXUVHeYhGphzysVz-gdV3JQ4c/edit?usp=drivesdk";
+const amount = 2000000;
 
-const getProposalRes = {
-   'additional-resource': {
-     type: 13,
-     data: 'https://docs.google.com/document/d/1OvPaYiKWuSkFCnEyufrXUVHeYhGphzysVz-gdV3JQ4c/edit?usp=drivesdk'
+const proposalResponse = {
+   "additional-resource": {
+      type: 13,
+      data: "https://docs.google.com/document/d/1OvPaYiKWuSkFCnEyufrXUVHeYhGphzysVz-gdV3JQ4c/edit?usp=drivesdk",
    },
-   'amount-gotten': { type: 1, value: 0n },
-   'amount-needed': { type: 1, value: 2500n },
+   "amount-gotten": { type: 1, value: 0n },
+   "amount-needed": { type: 1, value: 2500n },
    concluded: { type: 4 },
    description: {
-     type: 13,
-     data: 'Open Gov is a crowfunding platform on stacks blockchain'
+      type: 13,
+      data: "Open Gov is a crowfunding platform on stacks blockchain",
    },
    dislikes: { type: 1, value: 0n },
    id: { type: 1, value: 1n },
    likes: { type: 1, value: 0n },
-   niche: { type: 13, data: 'Governance' },
+   niche: { type: 13, data: "Governance" },
    proposer: {
-     type: 5,
-     address: {
-       type: 0,
-       version: 26,
-       hash160: '7321b74e2b6a7e949e6c4ad313035b1665095017'
-     }
+      type: 5,
+      address: {
+         type: 0,
+         version: 26,
+         hash160: "7321b74e2b6a7e949e6c4ad313035b1665095017",
+      },
    },
-   title: { type: 13, data: 'Open Gov' }
-}
- 
+   title: { type: 13, data: "Open Gov" },
+};
 
 describe("test create-proposal function", () => {
-  it("should successfully create a proposal", () => {
-    const { result } = simnet.callPublicFn(
-      'opengov',
-      'create-proposal',
-      [
-         Cl.stringAscii(title),
-         Cl.stringAscii(niche),
-         Cl.stringAscii(description),
-         Cl.uint(amountNeeded),
-         Cl.stringAscii(additionalResource)
-      ],
-      address1
-   )
+   it("should successfully create a proposal", () => {
+      const { result } = simnet.callPublicFn(
+         "opengov",
+         "create-proposal",
+         [
+            Cl.stringAscii(title),
+            Cl.stringAscii(niche),
+            Cl.stringAscii(problem),
+            Cl.stringAscii(solution),
+            Cl.stringAscii(milestone),
+            Cl.stringAscii(twitter),
+            Cl.stringAscii(discord),
+            Cl.uint(amountNeeded),
+            Cl.stringAscii(additionalResource),
+         ],
+         address1
+      );
 
-   expect(result).toBeOk(Cl.uint(id + 1))
+      expect(result).toBeOk(Cl.uint(id + 1));
+   });
+});
 
-   const getProposalResponse = simnet.callReadOnlyFn('opengov', 'get-proposal', [Cl.uint(id + 1)], address1)
+describe("test delegate function", () => {
+   // Create a proposal
+   it("should successfully create a proposal and allow other users to delegate to the proposal", () => {
+      const { result } = simnet.callPublicFn(
+         "opengov",
+         "create-proposal",
+         [
+            Cl.stringAscii(title),
+            Cl.stringAscii(niche),
+            Cl.stringAscii(problem),
+            Cl.stringAscii(solution),
+            Cl.stringAscii(milestone),
+            Cl.stringAscii(twitter),
+            Cl.stringAscii(discord),
+            Cl.uint(amountNeeded),
+            Cl.stringAscii(additionalResource),
+         ],
+         address1
+      );
 
-   expect(getProposalResponse.result.value.data).toStrictEqual(getProposalRes);
-  });
+      expect(result).toBeOk(Cl.uint(id + 1));
+
+      //  Delegate to the just created proposal
+      const delegateResponse = simnet.callPublicFn(
+         "opengov",
+         "delegate",
+         [Cl.uint(id + 1), Cl.uint(amount)],
+         address2
+      );
+
+      expect(delegateResponse.result).toBeOk(Cl.bool(true));
+   });
+
+   it("should successfully create a proposal and fail when the amount is zero", () => {
+      const { result } = simnet.callPublicFn(
+         "opengov",
+         "create-proposal",
+         [
+            Cl.stringAscii(title),
+            Cl.stringAscii(niche),
+            Cl.stringAscii(problem),
+            Cl.stringAscii(solution),
+            Cl.stringAscii(milestone),
+            Cl.stringAscii(twitter),
+            Cl.stringAscii(discord),
+            Cl.uint(amountNeeded),
+            Cl.stringAscii(additionalResource),
+         ],
+         address1
+      );
+
+      expect(result).toBeOk(Cl.uint(id + 1));
+
+      //  Delegate to the just created proposal
+      const delegateResponse = simnet.callPublicFn(
+         "opengov",
+         "delegate",
+         [Cl.uint(id + 1), Cl.uint(0)],
+         address2
+      );
+
+      expect(delegateResponse.result).toBeErr(Cl.uint(401));
+   });
 });
